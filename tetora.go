b2abf689b09b0f4bf77bd4d17d07729e529b0e7c
shinoda <tetora1053@gmail.com>
2019-07-20 22:56:36 +0900

    add Map func that receives a slice and a callback func and returns a new slice which consists of values made by the received callback func

--- tetora.go
+++ tetora.go
@@ -7,3 +7,12 @@ import (
 func Hello() {
 	fmt.Println("Hello, Tetora!")
 }
+
+func Map(arr []int, callback func(e int) int) []int {
+	var res_arr []int
+	for _, val := range arr {
+		res := callback(val)
+		res_arr = append(res_arr, res)
+	}
+	return res_arr
+}
